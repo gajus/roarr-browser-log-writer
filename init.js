6c05619aa9e97c53f4241b1e32c4613485b5ead8
Gajus Kuizinas <gajus@gajus.com>
2021-11-08 16:33:26 -0600

    fix: compile browser compatible version

--- init.js
+++ init.js
@@ -1,4 +1,4 @@
 const createLogWriter = require('./dist/src/factories/createLogWriter.js').createLogWriter;
 
-globalThis.ROARR = globalThis.ROARR ?? {};
+globalThis.ROARR = globalThis.ROARR ? {} : globalThis.ROARR;
 globalThis.ROARR.write = createLogWriter();
